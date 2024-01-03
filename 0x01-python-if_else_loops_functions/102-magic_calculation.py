256a9c60abfae63490b17d008b773498f2cf2c9c
Lynn <akichlynn@gmail.com>
2024-01-03 15:52:44 -0800

    Write the Python function def magic_calculation(a, b, c):

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author - Lynn Akich
+
+def magic_calculation(a, b, c):
+    """Matchbytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
+
